b65674193c4cc4b35f18a4dbf01d9e8bc6914aad
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-05-05 20:18:50 +0530

    added static mongoose method

--- routes/returns.js
+++ routes/returns.js
@@ -9,10 +9,8 @@ require("joi-objectid");
 const router = express.Router();
 
 router.post("/", [auth, validate(validateReturn)], async (req, res) => {
-	const rental = await Rental.findOne({
-		"customer._id": req.body.customerId,
-		"movie._id": req.body.movieId,
-	});
+	const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
+
 	if (!rental) return res.status(404).send("Rental not found");
 	if (rental.dateReturned)
 		return res.status(400).send("Rental is already processed");
